caccef007c80640e591ea45ee05ff68a6c908d77
ByungyeonKim <viva_kby@naver.com>
2021-12-27 17:15:05 +0900

    ts: 첫 사용, 글로벌 모듈 설치

---
+++ start/main.js
@@ -0,0 +1,7 @@
+console.log('Hello World~!! 와우!');
+var Person = /** @class */ (function () {
+    function Person() {
+        this.age = 1;
+    }
+    return Person;
+}());
---
+++ start/main.ts
@@ -0,0 +1,7 @@
+console.log('Hello World~!! 와우!');
+class Person {
+  age: number;
+  constructor() {
+    this.age = 1;
+  }
+}
