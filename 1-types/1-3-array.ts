5c858fe33051388bf0ffc031c0be33eba4389de4
ByungyeonKim <viva_kby@naver.com>
2021-12-28 11:42:42 +0900

    배열의 타입 정의, readonly, tuple

---
+++ 1-types/1-3-array.ts
@@ -0,0 +1,17 @@
+{
+  // Array
+  const fruits: string[] = ['🍓', 🍉];
+  const scores: Array<number> = [1, 2, 5];
+  // readonly는 아직 string[]의 형식만 지원한다. 2번째 방법은 ❎
+  function printArray(fruits: readonly string[]) {}
+
+  // Tuple -> interface, type alias, class로 대체하는 것이 좋다.
+  // 따라서 Tuple은 권장하지 않는다.
+  // 유연하게 잘 활용한 예시는 리액트 훅의 useState()이다.
+  // 동적으로 리턴하고, 여러 타입의 데이터가 존재하며 사용자가 이름을 정의해서 쓸 경우
+  let student: [string, number];
+  student = ['name', 123];
+  student[0] // name
+  student[1] // 123
+  const [name, age] = student;
+}
