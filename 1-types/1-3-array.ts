9badcf0b6f92637aa97a1d2d02066fe4ee7eb4e9
ByungyeonKim <viva_kby@naver.com>
2022-01-10 17:47:38 +0900

    에러 수정

--- 1-types/1-3-array.ts
+++ 1-types/1-3-array.ts
@@ -1,6 +1,6 @@
 {
   // Array
-  const fruits: string[] = ['🍓', 🍉];
+  const fruits: string[] = ['🍓', '🍉'];
   const scores: Array<number> = [1, 2, 5];
   // readonly는 아직 string[]의 형식만 지원한다. 2번째 방법은 ❎
   function printArray(fruits: readonly string[]) {}
@@ -11,7 +11,7 @@
   // 동적으로 리턴하고, 여러 타입의 데이터가 존재하며 사용자가 이름을 정의해서 쓸 경우
   let student: [string, number];
   student = ['name', 123];
-  student[0] // name
-  student[1] // 123
+  student[0]; // name
+  student[1]; // 123
   const [name, age] = student;
 }
